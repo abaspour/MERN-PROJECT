cc4301ebece0c54e150a95eaf5af3c6baae8cdc3
Mehdi <abbaspoor.mehdi@gmail.com>
2024-05-18 22:38:17 -0500

    add rabbitMQ

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import studentRoutes from './router/student.js';
+import mongoURI from './config/env.js';
+
 
 const app=express();
 app.use(cors());
@@ -21,8 +23,55 @@ app.use('/test',(req,res)=>{
 
 const db_url="mongodb+srv://<user>:<pass>@<cluster>.mongodb.net/?retryWrites=true&w=majority";
 const port=process.env.port || 5000;
-mongoose.connect(db_url,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology:true})
        .then(()=> app.listen(port,() => 
            console.log(`connection is established and running on port ${port}`)
         )).catch((err)=> console.log(err.message));
 // mongoose.set(`useFindAndModify`,false);        
+
+import amqp from 'amqplib';
+
+
+const connectToRabbitMQ = async () => {
+    try {
+        const connection = await amqp.connect('amqp://localhost:5672/');
+        const channel = await connection.createChannel();
+
+        // Now you can use the channel to send and receive messages
+        sendMessage(channel, 'myQueue', 'Hello RabbitMQ!'+Date());
+        consumeMessage(channel, 'myQueue');
+
+
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+    }
+}
+const sendMessage = async (channel, queueName, message) => {
+    try {
+        await channel.assertQueue(queueName);
+        await channel.sendToQueue(queueName, Buffer.from(message));
+        console.log('Message sent:', message);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+}
+
+// Call sendMessage function after the channel is created
+const consumeMessage = async (channel, queueName) => {
+    try {
+        await channel.assertQueue(queueName);
+        channel.consume(queueName, (message) => {
+            if (message !== null) {
+                console.log('Received message:', message.content.toString());
+                channel.ack(message);
+            }
+        });
+    } catch (error) {
+        console.error('Error consuming message:', error);
+    }
+}
+
+// Call consumeMessage function after the channel is created
+
+
+connectToRabbitMQ();
